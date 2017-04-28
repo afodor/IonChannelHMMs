803ec11d467a3643e8d78135b9397d0037af0f99
Anthony Fodor <anthony.fodor@gmail.com>
2017-04-28 14:47:11 -0400

    add a main method for Bounded dishonest...

--- IonChannelHMMs/src/markovChain/BoundedDishonestCasino.java
+++ IonChannelHMMs/src/markovChain/BoundedDishonestCasino.java
@@ -4,7 +4,7 @@ public class BoundedDishonestCasino extends DishonestCasinoModel
 {
 	public static final float TRANSITION_PROB_FROM_LAST_STATE = 0.0001f;
 	
-	private final static float[] PROBS_TO_INITIAL_STATE = { 0.7f, 0.3f };
+	private final static float[] PROBS_TO_INITIAL_STATE = { 0.5f, 0.5f };
 	private final static float[] PROBS_FROM_LAST_STATE = 
 		{ TRANSITION_PROB_FROM_LAST_STATE, TRANSITION_PROB_FROM_LAST_STATE};
 	
@@ -24,5 +24,32 @@ public class BoundedDishonestCasino extends DishonestCasinoModel
 	public float[] getTransitionProbsToTerminatingSilentState()
 	{
 		return PROBS_FROM_LAST_STATE;
+	}
+	
+	public static void main(String[] args) throws Exception
+	{
+		BoundedDishonestCasino model= new BoundedDishonestCasino();
+		String[] emissions = {"3","6","6"};
+		
+		ForwardAlgorithm fa = new ForwardAlgorithm(model, emissions);
+		BackwardsAlgorithm ba = new BackwardsAlgorithm(model, emissions);
+		
+		for( int x=0; x < emissions.length; x++)
+		{
+			System.out.print( emissions[ x ] );
+			
+			for(int y=0; y < model.getMarkovStates().length; y++)
+			{
+				double val = Math.exp( fa.getLogProbs()[y][x]) * Math.exp( ba.getLogProbs()[y][x])
+							/Math.exp(fa.getLogFinalPValue()) ;
+				System.out.print(" " +   val);
+			}
+			
+			System.out.println();
+		}
+		
+
+		System.out.println( Math.exp(fa.getLogFinalPValue()) );
+		System.out.println( Math.exp(ba.getLogFinalPValue()) );
 	}
 }
